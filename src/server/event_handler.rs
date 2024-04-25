211217e5be20330b55aa1b5ceddf126989f6228f
Veroni <zoubheir@gmail.com>
2024-04-25 22:04:46 +0200

    Version 1.3.3: Fix some bug

--- src/server/event_handler.rs
+++ src/server/event_handler.rs
@@ -4,7 +4,7 @@ use super::Public;
 
 #[async_trait::async_trait]
 pub trait EventHandler: Send + Sync {
-    async fn on_join(&mut self, request: RequestStruct, public: &mut Public);
-    async fn on_message(&mut self, public: &mut Public);
-    async fn on_close(&mut self);
+    async fn on_join(&self, request: RequestStruct, public: &mut Public);
+    async fn on_message(&self, public: &mut Public);
+    async fn on_close(&self);
 }
